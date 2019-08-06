c6fe76d66ad6d781f3ebde855b29af3407fc4f2d
中村　貫太 <sevenstars122@gmail.com>
2019-08-06 19:05:45 +0900

    finish todo

--- js/main.js
+++ js/main.js
@@ -5,16 +5,19 @@
     el: '#app',
     data: {
       newItem: '',
-      todos: [{
-        title: 'task1',
-        isDone: false
-      },{
-        title: 'task2',
-        isDone: false
-      },{
-        title: 'task3',
-        isDone: true
-      }]
+      todos: []
+    },
+    watch: {
+      todos: {
+        handler: function() {
+          localStorage.setItem('todos', JSON.stringify(this.todos));
+          alert('Data saved!');
+        },
+        deep: true
+      }
+    },
+    mounted: function() {
+      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
     },
     methods: {
       addItem: function() {
@@ -22,7 +25,7 @@
           title: this.newItem,
           isDone: false
         };
-        this.todos.push(this.newItem);
+        this.todos.push(item);
         this.newItem = '';
       },
       deleteItem: function(index) {
@@ -34,18 +37,11 @@
         if (!confirm('delete finished?')) {
           return;
         }
-        // this.todos = this.todos.filter(function(todo){
-        //   return !todo.isDone;
-        // });
         this.todos = this.remaining;
       }
     },
     computed: {
       remaining: function() {
-        // var items = this.todos.filter(function(todo){
-        //   return !todo.isDone;
-        // });
-        // return items.length;
         return this.todos.filter(function(todo) {
           return !todo.isDone;
         });
